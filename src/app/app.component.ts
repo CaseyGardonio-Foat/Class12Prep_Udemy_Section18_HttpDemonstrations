4d7b10e5ab016975d3825dcd0b6a59c0c56b049f
Casey <casey.gardonio@foat.org>
2021-01-20 16:37:20 -0600

    completed through Section 18 part 256

---
+++ src/app/app.component.ts
@@ -0,0 +1,54 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  loadedPosts = [];
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit() {
+    this.fetchPosts();
+  }
+
+  onCreatePost(postData: { title: string; content: string }) {
+    // Send Http request
+    this.http.post('https://class12-udemy18-19-default-rtdb.firebaseio.com/class12-udemy18-19-default-rtdb/class12-udemy18-19-default-rtdb.json', postData)
+    .subscribe(responseData=>{
+        console.log(responseData);
+    });
+  }
+
+  onFetchPosts() {
+    // Send Http request
+    this.fetchPosts();
+  }
+
+  onClearPosts() {
+    // Send Http request
+  }
+
+  private fetchPosts() {
+    this.http
+      .get('https://class12-udemy18-19-default-rtdb.firebaseio.com/class12-udemy18-19-default-rtdb/class12-udemy18-19-default-rtdb.json')
+      .pipe(
+        map(responseData =>{
+          const postsArray = [];
+          for(const key in responseData) {
+            if(responseData.hasOwnProperty(key)) {
+              postsArray.push({...responseData[key], id: key});
+            }
+          }
+          return postsArray;
+        })
+      )
+      .subscribe(posts =>{
+      console.log(posts);
+    });
+  }
+}
